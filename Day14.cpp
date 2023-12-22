2ae2f2213c034a7bf2e082c5c029f5bd40c665f7
Anne-Elise Chung <aelichung@gmail.com>
2023-12-22 15:42:17 -0800

    complete day 14 part 2
    not very dry, but a lil easier to read?

--- Day14.cpp
+++ Day14.cpp
@@ -2,64 +2,216 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <unordered_set>
 
-int main() {
-
-	std::ifstream ifs("input.txt");
-	std::string s;
-	std::vector<std::string> horizontalStrings;
-
-	while (ifs.good()) {
-		std::getline(ifs, s);
-		horizontalStrings.push_back(s);
-	}
-
-	std::vector<std::string> verticalStrings;
-
-	for (long long charIndex = 0; charIndex < horizontalStrings.at(0).length(); charIndex++) {
-		std::string vString;
-		for (long long rowIndex = 0; rowIndex < horizontalStrings.size(); rowIndex++) {
-			char c = horizontalStrings.at(rowIndex).at(charIndex);
-			vString += c;
+struct gridHash {
+	std::size_t operator()(std::vector<std::string> const& grid) const {
+		std::hash<std::string> hasher;
+		std::string hashString;
+		for (auto string : grid) {
+			hashString += string;
 		}
-		verticalStrings.push_back(vString);
+		return hasher(hashString);
 	}
-	
-	long long upperRow = verticalStrings.at(0).size();
-	long long sum = 0;
+};
 
-	for (long long i = 0; i < verticalStrings.size(); i++) {
+void TiltNorth(std::vector<std::string>& grid) {	
+	std::string firstRow = grid[0];
+	for (long long colIndex = 0; colIndex < firstRow.size(); colIndex++) 
+	{
 		long long maxUpperPosition = -1;
-		std::string columnString = verticalStrings[i];
-		for (long long j = 0; j < columnString.size(); j++) {
-			char c = columnString[j];
+		for (long long rowIndex = 0; rowIndex < grid.size(); rowIndex++) {
+			std::string row = grid[rowIndex];
+			char c = row[colIndex];
 			switch (c) {
 			case 'O':
-				//add the upmost free position to the sum
-				if (maxUpperPosition != -1) {
-					sum += upperRow - maxUpperPosition;
+				//switch the characters around in the string Grid and 
+				if (maxUpperPosition != -1 && maxUpperPosition != rowIndex) {
+					grid[maxUpperPosition][colIndex] = 'O';
+					grid[rowIndex][colIndex] = '.';
 					++maxUpperPosition;
 				}
-				//we are already at the upmost position because it hasn't been set yet, add the current row position to sum
-				else {
-					sum += upperRow - j;
-					maxUpperPosition = j + 1;
-				}
-				break;
-			case '.':
-				if (maxUpperPosition > j || maxUpperPosition == -1) {
-					maxUpperPosition = j;
+				//we are already at the upmost position because it hasn't been set yet
+				else {					
+					maxUpperPosition = rowIndex + 1;
 				}
 				break;
 			case '#':
-				maxUpperPosition = j + 1;
+				maxUpperPosition = rowIndex + 1;
+				break;
+			case '.':
+				if (maxUpperPosition > rowIndex || maxUpperPosition == -1) {
+					maxUpperPosition = rowIndex;
+				}
 				break;
 			}
 		}
 	}
+}
 
+void TiltWest(std::vector<std::string>& grid) {	
+	for (long long rowIndex = 0; rowIndex < grid.size(); rowIndex++) {
+		long long maxWestPosition = -1;
+		std::string row = grid[rowIndex];
+		for (long long colIndex = 0; colIndex < row.size(); colIndex++) {
+			char c = row[colIndex];
+			switch (c) {
+			case 'O':
+				//switch the characters around in the string Grid and 
+				if (maxWestPosition != -1 && maxWestPosition != colIndex) {
+					grid[rowIndex][maxWestPosition] = 'O';
+					grid[rowIndex][colIndex] = '.';
+					++maxWestPosition;
+				}
+				//we are already at the upmost position because it hasn't been set yet
+				else {
+					maxWestPosition = colIndex + 1;
+				}
+				break;
+			case '#':
+				maxWestPosition = colIndex + 1;
+				break;
+			case '.':
+				if (maxWestPosition > colIndex|| maxWestPosition == -1) {
+					maxWestPosition = colIndex;
+				}
+				break;
+			}
+		}
+	}
+}
+
+void TiltSouth(std::vector<std::string>& grid) {	
+	std::string firstRow = grid[0];
+	for (long long colIndex = 0; colIndex < firstRow.size(); colIndex++)
+	{
+		long long maxLowPosition = -1;
+		for (long long rowIndex = grid.size() - 1; rowIndex >= 0; rowIndex--) {
+			std::string row = grid[rowIndex];
+			char c = row[colIndex];
+			switch (c) {
+			case 'O':
+				//switch the characters around in the string Grid and 
+				if (maxLowPosition != -1 && maxLowPosition != rowIndex) {
+					grid[maxLowPosition][colIndex] = 'O';
+					grid[rowIndex][colIndex] = '.';
+					--maxLowPosition;
+				}
+				//we are already at the upmost position because it hasn't been set yet
+				else {
+					maxLowPosition = rowIndex - 1;
+				}
+				break;
+			case '#':
+				maxLowPosition = rowIndex - 1;
+				break;
+			case '.':
+				if (maxLowPosition < rowIndex || maxLowPosition == -1) {
+					maxLowPosition = rowIndex;
+				}
+				break;
+			}
+		}
+	}
+}
+
+void TiltEast(std::vector<std::string>& grid) {
+	for (long long rowIndex = 0; rowIndex < grid.size(); rowIndex++) {
+		long long maxEastPosition = -1;
+		std::string row = grid[rowIndex];
+		for (long long colIndex = row.size() - 1; colIndex >= 0; colIndex--) {
+			char c = row[colIndex];
+			switch (c) {
+			case 'O':
+				//switch the characters around in the string Grid and 
+				if (maxEastPosition != -1 && maxEastPosition != colIndex) {
+					grid[rowIndex][maxEastPosition] = 'O';
+					grid[rowIndex][colIndex] = '.';
+					--maxEastPosition;
+				}
+				//we are already at the upmost position because it hasn't been set yet
+				else {
+					maxEastPosition = colIndex - 1;
+				}
+				break;
+			case '#':
+				maxEastPosition = colIndex - 1;
+				break;
+			case '.':
+				if (maxEastPosition < colIndex || maxEastPosition == -1) {
+					maxEastPosition = colIndex;
+				}
+				break;
+			}
+		}
+	}
+}
+
+void Cycle(std::vector<std::string>& grid) {
+	TiltNorth(grid);
+	TiltWest(grid);
+	TiltSouth(grid);
+	TiltEast(grid);
+}
+
+int main() {
+
+	std::ifstream ifs("input.txt");
+	std::string s;	
+	long long rowCount = 0;
+
+	std::vector<std::string> strings;
+	while (ifs.good()) {
+		std::getline(ifs, s);		
+
+		strings.push_back(s);
+
+		rowCount++;
+	}
 	ifs.close();
 
-	std::cout << "sum: " << sum << "\n";
+	long long cycleCount = 1;
+	std::map<std::vector<std::string>, long long> gridMap;
+	std::map<long long, std::vector<std::string>> cycleMap;
+
+	Cycle(strings);
+
+	while (!gridMap.contains(strings)) {
+		gridMap[strings] = cycleCount;
+		cycleMap[cycleCount] = strings;
+
+		Cycle(strings);
+		++cycleCount;
+	}
+
+	//length of loop in number of cycles
+	long long loopLength = cycleCount - gridMap[strings];
+	long long remainderCycles = (1000000000 - gridMap[strings]) % loopLength;
+
+	std::vector<std::string> billionthGrid;
+	if (remainderCycles == 0) {
+		billionthGrid = strings;
+	}
+	else {
+		long long billionthCycle = remainderCycles + gridMap[strings];
+		billionthGrid = cycleMap[billionthCycle];
+	}
+
+	long long loadSum = 0;
+	long long upper = billionthGrid.size();
+	//calculate load on billionth Grid
+	for (long long rowIndex = 0; rowIndex < billionthGrid.size(); rowIndex++) {		
+		std::string row = billionthGrid[rowIndex];
+		for (long long colIndex = 0; colIndex < row.size(); colIndex++) {
+			char c = row[colIndex];
+			if (c == 'O') {
+				loadSum += upper - rowIndex;
+			}
+		}
+	}
+
+
+	std::cout << "sum: " << loadSum << "\n";
 	return 0;
 }
