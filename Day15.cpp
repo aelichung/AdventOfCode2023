a243b3882030613446f3c474db4c87e4326b2fea
Anne-Elise Chung <aelichung@gmail.com>
2023-12-25 22:01:10 -0800

    complete day 15 part 2

--- Day15.cpp
+++ Day15.cpp
@@ -1,20 +1,9 @@
-//rn = 1 becomes 30.
-//cm - becomes 253.
-//qp = 3 becomes 97.
-//cm = 2 becomes 47.
-//qp - becomes 14.
-//pc = 4 becomes 180.
-//ot = 9 becomes 9.
-//ab = 5 becomes 197.
-//pc - becomes 48.
-//pc = 6 becomes 214.
-//ot = 7 becomes 231.
-//In this example, the sum of these results is 1320.
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <regex>
 
 long long HASH(std::string input) {
 	long long currentValue = 0;
@@ -27,6 +16,51 @@ long long HASH(std::string input) {
 	return currentValue;
 }
 
+void HASHMAP(std::map<int, std::vector<std::string>>& boxes, std::string label, char operation, std::string focalLengthString = "") {
+	//get box number
+	long long boxNum = HASH(label);
+	std::vector<std::string>& box = boxes[boxNum];
+	long long lensPosition = -1;
+
+	switch (operation) {
+	case '-':	
+		for (long long i = 0; i < box.size(); i++) {
+			auto s = box[i];
+			std::string boxLabel = s.substr(0, s.find(" "));
+			if (boxLabel == label) {
+				lensPosition = i;
+				break;
+			}
+		}
+		if (lensPosition != -1) {
+			box.erase(box.begin() + lensPosition);
+		}
+		break;
+	case '=':		
+		for (long long i = 0; i < box.size(); i++) {
+			auto s = box[i];
+			std::string boxLabel = s.substr(0, s.find(" "));
+			if (boxLabel == label) {
+				lensPosition = i;
+				break;
+			}
+		}
+		std::string newLabel = label + " " + focalLengthString;
+		if (lensPosition != -1) {
+			box[lensPosition] = newLabel;
+		}
+		else {
+			box.push_back(newLabel);
+		}
+		break;
+	}
+	//boxes[boxNum] = box;
+}
+
+long long GetFocusingPower(long long boxNumber, long long lensSlotNumber, long long focalLength) {
+	return (boxNumber + 1) * lensSlotNumber * focalLength;
+}
+
 int main() {
 	std::ifstream ifs("input.txt");
 	std::string s;
@@ -37,21 +71,63 @@ int main() {
 	std::getline(ifs, s);
 	ifs.close();
 		
-	auto delimiterPosition = s.find(delimiter);
-	while (delimiterPosition != std::string::npos) {
-		std::string token = s.substr(findPosition, delimiterPosition - findPosition);
-		long long hash = HASH(token);
-		sum += hash;
-		findPosition = delimiterPosition + 1;
-		delimiterPosition = s.find(delimiter, findPosition);
-		std::cout << token <<": hash is "<< hash << '\n';
+	//part1
+	//auto delimiterPosition = s.find(delimiter);
+	//while (delimiterPosition != std::string::npos) {
+	//	std::string token = s.substr(findPosition, delimiterPosition - findPosition);
+	//	long long hash = HASH(token);
+	//	sum += hash;
+	//	findPosition = delimiterPosition + 1;
+	//	delimiterPosition = s.find(delimiter, findPosition);
+	//	std::cout << token <<": hash is "<< hash << '\n';
+	//}
+
+	//std::string token = s.substr(findPosition, s.length() - findPosition);
+	//long long hash = HASH(token);
+	//std::cout << token << ": hash is " << hash << '\n';
+	//sum += hash;
+
+	std::map<int, std::vector<std::string>> boxes;
+	int counter = 0;
+	while (counter < 256) {
+		auto vec = std::vector<std::string>();
+		boxes[counter] = vec;
+		++counter;
 	}
 
-	std::string token = s.substr(findPosition, s.length() - findPosition);
-	long long hash = HASH(token);
-	std::cout << token << ": hash is " << hash << '\n';
-	sum += hash;
-		
+	std::regex boxPattern(R"(([a-z]+)([=-])(\d*),?)");
+	std::sregex_iterator begin = std::sregex_iterator(s.begin(), s.end(), boxPattern);
+	auto end = std::sregex_iterator();
+	
+	for (std::sregex_iterator i = begin; i != end; i++) {
+		std::smatch match = *i;
+		std::string label = match[1];
+		std::string operation = match[2];
+		std::string focalLength = match[3];
+		//std::cout << "label: " << label << " , operation: " << operation << ", focal length: " << focalLength << '\n';
+		HASHMAP(boxes, label, operation[0], focalLength);
+	}
+
+	for (long long i = 0; i < boxes.size(); i++) {
+		std::string boxContents;
+		for (auto s : boxes[i]) {
+			boxContents += s + " ";
+		}
+		std::cout << " box " << i << ": " << boxContents << '\n';
+		std::vector<std::string> box = boxes[i];
+
+		if (box.empty()) {
+			continue;
+		}
+		for (long long lensIndex = 1; lensIndex <= box.size(); lensIndex++) {			
+			std::string labelString = box[lensIndex - 1];
+			char c = labelString[labelString.length() - 1];
+			std::string focalLengthString{c};
+			long long focalLength = std::stoll(focalLengthString);
+			sum += GetFocusingPower(i, lensIndex, focalLength);
+		}
+	}
+	
 	std::cout << "sum: " << sum << '\n';
 	return 0;
 }
